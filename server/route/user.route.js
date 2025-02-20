547574fe1e4d73775f5079a0b06a257d76524b20
Mariappan R <mariappanr2004@gmail.com>
2025-02-20 21:10:16 +0530

    Add files via upload

---
+++ server/route/user.route.js
@@ -0,0 +1,19 @@
+import { Router } from 'express'
+import { loginController, logoutController, registerUserController, uploadAvatar, verifyEmailController, updateUserDetails, forgotPasswordController, verifyForgotPasswordOtp, resetpassword, refreshToken } from '../controllers/user.controller.js'
+import auth from '../middleware/auth.js'
+import upload from '../middleware/multer.js'
+
+const userRouter = Router()
+
+userRouter.post('/register',registerUserController)
+userRouter.post('/verify-email',verifyEmailController)
+userRouter.post('/login',loginController)
+userRouter.get('/logout',auth,logoutController)
+userRouter.put('/upload-avatar',auth,upload.single('avatar'),uploadAvatar)
+userRouter.put('/update-user', auth,updateUserDetails)
+userRouter.put('/forgot-password', forgotPasswordController)
+userRouter.put('/verify-forgot-password-otp', verifyForgotPasswordOtp)
+userRouter.put('/reset-password', resetpassword)
+userRouter.post('/refresh-token',refreshToken)
+
+export default userRouter
